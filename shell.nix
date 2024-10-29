163c713e47b57b5bbb7bcc08967e2d86114cf523
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-10-28 22:26:07 -0400

    update deps

--- shell.nix
+++ shell.nix
@@ -1,6 +1,8 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShellNoCC {
   packages = with pkgs; [
+    python312Packages.pip
+    pipx
     mkdocs
     python312Packages.mkdocs-material
     (pkgs.python3.withPackages (python-pkgs:
