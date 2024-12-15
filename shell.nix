b414f4c7273c7c5ae78ce46cf8f8e2efd06c16e5
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-12-15 18:01:04 -0500

    remove python dependencies

--- shell.nix
+++ shell.nix
@@ -1,15 +1,6 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.mkShellNoCC {
   packages = with pkgs; [
-    python312Packages.pip
-    pipx
-    mkdocs
-    python312Packages.mkdocs-material
-    (pkgs.python3.withPackages (python-pkgs:
-      with python-pkgs; [
-        # select Python packages here
-        mkdocs
-        mkdocs-material
-      ]))
+    nodejs
   ];
 }
